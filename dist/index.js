24a9aa803051cc8eb3d974f66cf929980757d2fd
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-04-10 20:59:59 -0500

    solve can't use import outside of a module BS

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,51 +8,105 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import { launch } from "puppeteer";
-import Printer from "./printer";
-const NetworkAdapter = require("escpos-network");
-const SerialAdapter = require("escpos-serialport");
-const USBAdapter = require("escpos-usb");
-let browser;
-export const getBrowser = () => {
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CreateUSBPrinter = exports.CreateSerialPrinter = exports.CreateNetworkPrinter = exports.screenshotDomElement = exports.closeBrowser = exports.launchBrowser = exports.getBrowser = void 0;
+var puppeteer_1 = require("puppeteer");
+var printer_1 = require("./printer");
+var NetworkAdapter = require("escpos-network");
+var SerialAdapter = require("escpos-serialport");
+var USBAdapter = require("escpos-usb");
+var browser;
+var getBrowser = function () {
     return browser;
 };
+exports.getBrowser = getBrowser;
 /**
  * start the internal chromium browser
  */
-export const launchBrowser = () => __awaiter(void 0, void 0, void 0, function* () {
-    if (!browser) {
-        console.info('Launching Headless Chrome');
-        try {
-            if (process.platform === 'linux') {
+var launchBrowser = function () { return __awaiter(void 0, void 0, void 0, function () {
+    var e_1;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                if (!!browser) return [3 /*break*/, 8];
+                console.info('Launching Headless Chrome');
+                _a.label = 1;
+            case 1:
+                _a.trys.push([1, 6, , 7]);
+                if (!(process.platform === 'linux')) return [3 /*break*/, 3];
+                return [4 /*yield*/, puppeteer_1.launch({ executablePath: '/usr/bin/chromium-browser', args: ['--no-sandbox'] })];
+            case 2:
                 //linux needs to install chromium separately as of 2019, the automatically installed chromium didn't work on raspberry pi.
-                browser = yield launch({ executablePath: '/usr/bin/chromium-browser', args: ['--no-sandbox'] });
-            }
-            else {
-                browser = yield launch();
-            }
+                browser = _a.sent();
+                return [3 /*break*/, 5];
+            case 3: return [4 /*yield*/, puppeteer_1.launch()];
+            case 4:
+                browser = _a.sent();
+                _a.label = 5;
+            case 5: return [3 /*break*/, 7];
+            case 6:
+                e_1 = _a.sent();
+                console.error("failed to launch puppeteer browser.");
+                console.error(e_1);
+                return [3 /*break*/, 7];
+            case 7:
+                console.info('Headless Chrome Launched');
+                _a.label = 8;
+            case 8: return [2 /*return*/];
         }
-        catch (e) {
-            console.error("failed to launch puppeteer browser.");
-            console.error(e);
-        }
-        console.info('Headless Chrome Launched');
-    }
-});
+    });
+}); };
+exports.launchBrowser = launchBrowser;
 /**
  * shut down the internal chromium browser
  */
-export const closeBrowser = () => __awaiter(void 0, void 0, void 0, function* () {
-    if (browser) {
-        console.info("closing internal chromium browser.");
-        yield browser.close();
-        browser = null;
-        console.info("Internal chromium browser closed.");
-    }
-    else {
-        console.error("There is no active browser.");
-    }
-});
+var closeBrowser = function () { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                if (!browser) return [3 /*break*/, 2];
+                console.info("closing internal chromium browser.");
+                return [4 /*yield*/, browser.close()];
+            case 1:
+                _a.sent();
+                browser = null;
+                console.info("Internal chromium browser closed.");
+                return [3 /*break*/, 3];
+            case 2:
+                console.error("There is no active browser.");
+                _a.label = 3;
+            case 3: return [2 /*return*/];
+        }
+    });
+}); };
+exports.closeBrowser = closeBrowser;
 /**
  *
  * @param page Puppeteer page, the page to take screenshot on.
@@ -59,34 +114,50 @@ export const closeBrowser = () => __awaiter(void 0, void 0, void 0, function* ()
  * @param padding padding around the element, in pixels, default is 0, other element may show in the padding area.
  * @returns the image in a buffer.
  */
-export const screenshotDomElement = (page, selector, padding = 0) => __awaiter(void 0, void 0, void 0, function* () {
-    if (!selector)
-        throw "Please provide a selector.";
-    //get dimension of the selected element
-    const rect = yield page.evaluate(selector => {
-        const element = document.querySelector(selector);
-        if (!element)
-            return null;
-        const { x, y, width, height } = element.getBoundingClientRect();
-        return { left: x, top: y, width, height, id: element.id };
-    }, selector);
-    if (!rect)
-        throw Error(`Could not find element that matches selector: ${selector}.`);
-    //adjust viewport so the element adjust to the correct size
-    yield page.setViewport({
-        width: rect.width + padding * 2,
-        height: rect.height + padding * 2
+var screenshotDomElement = function (page, selector, padding) {
+    if (padding === void 0) { padding = 0; }
+    return __awaiter(void 0, void 0, void 0, function () {
+        var rect;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    if (!selector)
+                        throw "Please provide a selector.";
+                    return [4 /*yield*/, page.evaluate(function (selector) {
+                            var element = document.querySelector(selector);
+                            if (!element)
+                                return null;
+                            var _a = element.getBoundingClientRect(), x = _a.x, y = _a.y, width = _a.width, height = _a.height;
+                            return { left: x, top: y, width: width, height: height, id: element.id };
+                        }, selector)];
+                case 1:
+                    rect = _a.sent();
+                    if (!rect)
+                        throw Error("Could not find element that matches selector: " + selector + ".");
+                    //adjust viewport so the element adjust to the correct size
+                    return [4 /*yield*/, page.setViewport({
+                            width: rect.width + padding * 2,
+                            height: rect.height + padding * 2
+                        })];
+                case 2:
+                    //adjust viewport so the element adjust to the correct size
+                    _a.sent();
+                    return [4 /*yield*/, page.screenshot({
+                            clip: {
+                                x: rect.left - padding,
+                                y: rect.top - padding,
+                                width: rect.width + padding * 2,
+                                height: rect.height + padding * 2
+                            }
+                        })];
+                case 3: 
+                //take the screenshot
+                return [2 /*return*/, _a.sent()];
+            }
+        });
     });
-    //take the screenshot
-    return yield page.screenshot({
-        clip: {
-            x: rect.left - padding,
-            y: rect.top - padding,
-            width: rect.width + padding * 2,
-            height: rect.height + padding * 2
-        }
-    });
-});
+};
+exports.screenshotDomElement = screenshotDomElement;
 /**
  *
  * @param ip the ip address to connect to the printer
@@ -94,28 +165,35 @@ export const screenshotDomElement = (page, selector, padding = 0) => __awaiter(v
  * @param options printer options, see https://www.npmjs.com/package/escpos for detail, or just print out "printer.escposPrinter" too see what options are available
  * @returns
  */
-export const CreateNetworkPrinter = (ip, port = 9100, options = {}) => {
-    return new Printer(new NetworkAdapter(ip, port), options);
+var CreateNetworkPrinter = function (ip, port, options) {
+    if (port === void 0) { port = 9100; }
+    if (options === void 0) { options = {}; }
+    return new printer_1.default(new NetworkAdapter(ip, port), options);
 };
+exports.CreateNetworkPrinter = CreateNetworkPrinter;
 /**
  * @param pid port id, for example '/dev/usb/lp0'
  * @param options printer options, see https://www.npmjs.com/package/escpos for detail, or just print out "printer.escposPrinter" too see what options are available
  * @returns
  */
-export const CreateSerialPrinter = (pid, options = {}) => {
-    return new Printer(new SerialAdapter(pid), options);
+var CreateSerialPrinter = function (pid, options) {
+    if (options === void 0) { options = {}; }
+    return new printer_1.default(new SerialAdapter(pid), options);
 };
-export const CreateUSBPrinter = (vid, pid, options = {}) => {
+exports.CreateSerialPrinter = CreateSerialPrinter;
+var CreateUSBPrinter = function (vid, pid, options) {
+    if (options === void 0) { options = {}; }
     //TODO: implement a way to find printers with https://github.com/song940/node-escpos/blob/v3/packages/usb/README.md
-    return new Printer(new USBAdapter(vid, pid), options);
+    return new printer_1.default(new USBAdapter(vid, pid), options);
 };
-const htmltoescpos = {
-    CreateNetworkPrinter,
-    CreateSerialPrinter,
-    CreateUSBPrinter,
-    screenshotDomElement,
-    closeBrowser,
-    launchBrowser
+exports.CreateUSBPrinter = CreateUSBPrinter;
+var htmltoescpos = {
+    CreateNetworkPrinter: exports.CreateNetworkPrinter,
+    CreateSerialPrinter: exports.CreateSerialPrinter,
+    CreateUSBPrinter: exports.CreateUSBPrinter,
+    screenshotDomElement: exports.screenshotDomElement,
+    closeBrowser: exports.closeBrowser,
+    launchBrowser: exports.launchBrowser
 };
-export default htmltoescpos;
+exports.default = htmltoescpos;
 //# sourceMappingURL=index.js.map
--- dist/printer.js
+++ dist/printer.js
@@ -1,3 +1,15 @@
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -6,6 +18,33 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
 };
 var __rest = (this && this.__rest) || function (s, e) {
     var t = {};
@@ -18,25 +57,28 @@ var __rest = (this && this.__rest) || function (s, e) {
         }
     return t;
 };
-const escpos = require("escpos");
-const getPixels = require("get-pixels");
-import { getBrowser, launchBrowser, screenshotDomElement } from ".";
+Object.defineProperty(exports, "__esModule", { value: true });
+var escpos = require("escpos");
+var getPixels = require("get-pixels");
+var _1 = require(".");
 /**
  * you can use adapter and escpostPrinter field to issue custom commands as well, see https://github.com/song940/node-escpos for details
  */
-export default class Printer {
-    constructor(adapter, options = {}) {
+var Printer = /** @class */ (function () {
+    function Printer(adapter, options) {
+        var _this = this;
+        if (options === void 0) { options = {}; }
         /**
          * send a beep sequence to the buzzer
          */
-        this.beep = (options) => {
-            this.escposPrinter.beep(options.count, options.time);
+        this.beep = function (options) {
+            _this.escposPrinter.beep(options.count, options.time);
         };
         /**
          * open cashdraw on a specific pin
          */
-        this.cashdraw = (pin) => {
-            this.escposPrinter.cashdraw(pin);
+        this.cashdraw = function (pin) {
+            _this.escposPrinter.cashdraw(pin);
         };
         /**
          * print the given url. <see cref="PrintImageOptions"/>
@@ -45,51 +87,78 @@ export default class Printer {
          * @see PrintUrlOptions
          * @see PrintImageOptions
          */
-        this.printUrl = (url, _a = {}) => __awaiter(this, void 0, void 0, function* () {
-            var { pageWidth: viewportWidth = 575, pageHeight: viewportHeight = 600, waitForFunction, selector = 'body' } = _a, restOfOptions = __rest(_a, ["pageWidth", "pageHeight", "waitForFunction", "selector"]);
-            //make sure the internal browser is open
-            yield launchBrowser();
-            let page;
-            try {
-                //open a new page
-                page = yield getBrowser().newPage();
-                //set viewport
-                yield page.setViewport({ width: viewportWidth, height: viewportHeight });
-                //go to the url
-                yield page.goto(url);
-                //wait for the waitForFunction to become true
-                if (waitForFunction)
-                    yield page.waitForFunction(waitForFunction);
-                //screenshot the selected element or the whole body
-                const screen = yield screenshotDomElement(page, selector);
-                //print the screenshot
-                this.printImage(screen, Object.assign({}, restOfOptions));
-            }
-            catch (e) {
-                console.error(e.stack);
-                return e;
-            }
-            finally {
-                //make sure page is closed.
-                if (page) {
-                    page.close();
-                }
-            }
-        });
+        this.printUrl = function (url, _a) {
+            if (_a === void 0) { _a = {}; }
+            return __awaiter(_this, void 0, void 0, function () {
+                var page, screen_1, e_1;
+                var _b = _a.pageWidth, viewportWidth = _b === void 0 ? 575 : _b, _c = _a.pageHeight, viewportHeight = _c === void 0 ? 600 : _c, waitForFunction = _a.waitForFunction, _d = _a.selector, selector = _d === void 0 ? 'body' : _d, restOfOptions = __rest(_a, ["pageWidth", "pageHeight", "waitForFunction", "selector"]);
+                return __generator(this, function (_e) {
+                    switch (_e.label) {
+                        case 0: 
+                        //make sure the internal browser is open
+                        return [4 /*yield*/, _1.launchBrowser()];
+                        case 1:
+                            //make sure the internal browser is open
+                            _e.sent();
+                            _e.label = 2;
+                        case 2:
+                            _e.trys.push([2, 9, 10, 11]);
+                            return [4 /*yield*/, _1.getBrowser().newPage()];
+                        case 3:
+                            //open a new page
+                            page = _e.sent();
+                            //set viewport
+                            return [4 /*yield*/, page.setViewport({ width: viewportWidth, height: viewportHeight })];
+                        case 4:
+                            //set viewport
+                            _e.sent();
+                            //go to the url
+                            return [4 /*yield*/, page.goto(url)];
+                        case 5:
+                            //go to the url
+                            _e.sent();
+                            if (!waitForFunction) return [3 /*break*/, 7];
+                            return [4 /*yield*/, page.waitForFunction(waitForFunction)];
+                        case 6:
+                            _e.sent();
+                            _e.label = 7;
+                        case 7: return [4 /*yield*/, _1.screenshotDomElement(page, selector)];
+                        case 8:
+                            screen_1 = _e.sent();
+                            //print the screenshot
+                            this.printImage(screen_1, __assign({}, restOfOptions));
+                            return [3 /*break*/, 11];
+                        case 9:
+                            e_1 = _e.sent();
+                            console.error(e_1.stack);
+                            return [2 /*return*/, e_1];
+                        case 10:
+                            //make sure page is closed.
+                            if (page) {
+                                page.close();
+                            }
+                            return [7 /*endfinally*/];
+                        case 11: return [2 /*return*/];
+                    }
+                });
+            });
+        };
         this.adapter = adapter;
         this.escposPrinter = new escpos.Printer(adapter, options);
     }
     /**
      * print the given image, image should be a buffer or an url
      */
-    printImage(image, { topFeed = 2, bottomFeed = 2, cut = true, beep, cashdraw, imageFormat = "png" } = {}) {
+    Printer.prototype.printImage = function (image, _a) {
+        var _this = this;
+        var _b = _a === void 0 ? {} : _a, _c = _b.topFeed, topFeed = _c === void 0 ? 2 : _c, _d = _b.bottomFeed, bottomFeed = _d === void 0 ? 2 : _d, _e = _b.cut, cut = _e === void 0 ? true : _e, beep = _b.beep, cashdraw = _b.cashdraw, _f = _b.imageFormat, imageFormat = _f === void 0 ? "png" : _f;
         //translate the buffer or url into pixels
-        getPixels(image, "image/" + imageFormat, (err, pixels) => {
+        getPixels(image, "image/" + imageFormat, function (err, pixels) {
             if (err) {
                 console.error(err);
             }
             else {
-                this.adapter.open(adapterErr => {
+                _this.adapter.open(function (adapterErr) {
                     if (adapterErr) {
                         console.error(adapterErr);
                     }
@@ -97,27 +166,29 @@ export default class Printer {
                         //the printing process:
                         //feed top
                         if (topFeed)
-                            this.escposPrinter.feed(topFeed);
+                            _this.escposPrinter.feed(topFeed);
                         //print the given image in middle of the paper
-                        this.escposPrinter.align("ct").raster(new escpos.Image(pixels), null);
+                        _this.escposPrinter.align("ct").raster(new escpos.Image(pixels), null);
                         //feed bottom
                         if (bottomFeed)
-                            this.escposPrinter.feed(bottomFeed);
+                            _this.escposPrinter.feed(bottomFeed);
                         //cut paper
                         if (cut)
-                            this.escposPrinter.cut();
+                            _this.escposPrinter.cut();
                         //use buzzer
                         if (beep)
-                            this.beep(beep);
+                            _this.beep(beep);
                         //open connected cashdrawer
                         if (cashdraw)
-                            this.cashdraw(cashdraw);
+                            _this.cashdraw(cashdraw);
                         //flush commands and close connection
-                        this.escposPrinter.close();
+                        _this.escposPrinter.close();
                     }
                 });
             }
         });
-    }
-}
+    };
+    return Printer;
+}());
+exports.default = Printer;
 //# sourceMappingURL=printer.js.map
